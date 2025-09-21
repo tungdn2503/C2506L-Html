dbeb959d41a15a65e9cdd2d727677a3bc255910b
Tungdn2503 <tungdn2503@gmail.com>
2025-09-22 00:01:43 +0700

    Thêm bài tap btvn7-html

---
+++ Btvn7-html/script.js
@@ -0,0 +1,14 @@
+let a = 2;
+let b = 3;
+
+let tong = a + b;
+let hieu = a - b;
+let tich = a * b;
+let thuong = a / b;
+let phanDu = a % b;
+
+console.log("Tổng:", tong);
+console.log("Hiệu:", hieu);
+console.log("Tích:", tich);
+console.log("Thương:", thuong);
+console.log("Phần dư:", phanDu);
---
+++ Btvn7-html/script1.js
@@ -0,0 +1,7 @@
+let a = 2;
+let b = 3;
+
+let ketQua = a ** b;
+console.log("Giá trị của a:", a);
+console.log("Giá trị của b:", b);
+console.log(`Kết quả ${a}^${b} = ${ketQua}`);
